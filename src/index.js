b7594caf1824444cf3e09c986ffb0319f14530db
Mikhail Naryvonchyk <bel4elz@gmail.com>
2023-02-07 18:11:11 +0300

    init: start task, 65% effectiveness

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let objectConfig = Object.fromEntries(bracketsConfig);
+  
+  objectConfig = closeOpen(objectConfig);
+    function closeOpen(object) {
+      const res = {};
+      Object.keys(object).forEach(function(value) {
+        let key = object[value];
+        res[key] = value;
+      });
+      return res;
+    };
+  
+  
+  let openBrackets = Object.values(objectConfig);
+
+  let stack = [];
+  
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+  
+    if (openBrackets.includes(currentChar)) {
+      stack.push(currentChar);
+
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      
+      let topStackElement = stack[stack.length-1];
+
+      if (objectConfig[currentChar] === topStackElement) {
+      stack.pop();
+      } else {
+        return false;
+    }
+    } 
+  }
+      
+
+
+  return stack.length === 0;
+};
